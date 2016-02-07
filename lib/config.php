d3f65b501d4ce4e9ac45eced09e2de2128408e63
yunyun.cai <1835373375@qq.com>
2016-02-07 18:10:31 +0800

    mvc

---
+++ lib/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('HOSTNAME', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', '123456');
+define('DATABASE', 'test');
+define('PREFIX','');
+include 'lib/Control/CoreControl.php';
+?>
